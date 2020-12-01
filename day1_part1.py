2f2780197e270f9924bc0bd4688470db5d35684a
iona-evans <iona-kathryn.evans@bjss.com>
2020-12-01 14:27:52 +0000

    first commit

---
+++ day1_part1.py
@@ -0,0 +1,29 @@
+
+# coding: utf-8
+
+input_file = open("puzzleinput.txt")
+
+list1 = list(range(0))
+
+line = input_file.readline()
+res = 0
+while line != "":
+	num = int(line)
+	print (num)
+	res = 2020 - num
+	line = input_file.readline()
+	list1.append(res)
+
+result = list(range(0))
+
+
+input_file = open("puzzleinput.txt")
+line = input_file.readline()
+
+while line != "":
+    for number in list1:
+	    if int(line) == int(number):
+                result.append(line)
+    line = input_file.readline()
+
+print (result)
---
+++ day1_part2.py
@@ -0,0 +1,41 @@
+input_file = open("puzzleinput.txt")
+
+list1 = list(range(0))
+
+line = input_file.readline()
+
+while line != "":
+    num = int(line)
+    list1.append(num)
+    line = input_file.readline()
+
+print (list1)
+print (len(list1))
+
+def find3Numbers(A, arr_size, total):
+    A.sort()
+    for i in range(0, arr_size - 2):
+
+        l = i + 1
+
+        r = arr_size - 1
+        while l < r:
+
+            if A[i] + A[l] + A[r] == total:
+                print("Triplet is", A[i],
+                      ', ', A[l], ', ', A[r])
+                return True
+
+            elif A[i] + A[l] + A[r] < total:
+                l += 1
+            else:  
+                r -= 1
+
+    return False
+
+
+A = list1
+total = 2020
+arr_size = len(A)
+
+find3Numbers(A, arr_size, total)
